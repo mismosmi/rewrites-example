06a2e9e597c2ff3e382131ad28d776d4bce9f9c5
Michel Smola <michel.smola@farbenmeer.de>
2021-09-14 09:21:45 +0200

    add fallback rewrite

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+
+  async rewrites() {
+    return {
+      fallback: [
+        {
+          source: "/:path*",
+          destination: "https://vercel.com/:path*",
+        },
+      ],
+    };
+  },
+};
